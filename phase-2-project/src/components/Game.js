41199a246e835b48b3acba48fd3f31acdf880491
Drew <drwomble27@gmail.com>
2023-05-16 19:38:35 -0600

    commit to pull

--- phase-2-project/src/components/Game.js
+++ phase-2-project/src/components/Game.js
@@ -4,11 +4,23 @@ import Cards from "./Cards"
 function Game ({shuffledCards}) {
     //TODO take shuffled cards and display them in a grid
     //TODO on click start game then flip cards and set turns to 0
-    //TODO allow user to select 2 cards then 
-    //TODO
+    //TODO allow user to select 2 cards then if they match keep them up 
+    //send to matchedCards if they don't match flip them back over
+    //TODO 
+    const displayCards = shuffledCards.map(card => <Cards 
+      card={card}  key={card.id}/>)
+    const [startGame, setStartGame] = useState(false)
+
+    const handleStartGame = () => setStartGame(value => !value)
+    startGame ? 
     return(
         <div>
-            
+            <button className="start-button" onClick={handleStartGame}> 
+                Start Game
+            </button>
+            <div className="game-container">
+                {displayCards}
+            </div>
         </div>
     )
 }
