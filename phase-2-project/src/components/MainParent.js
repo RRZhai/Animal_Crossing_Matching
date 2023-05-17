30914aea03d8b1661be4f8b3130dfc165952790c
Drew <drwomble27@gmail.com>
2023-05-16 18:12:31 -0600

    commiting because i forgot to switch to my branch

--- phase-2-project/src/components/MainParent.js
+++ phase-2-project/src/components/MainParent.js
@@ -1,20 +1,24 @@
 import React from "react"
 import {useState, useEffect} from "react"
-import CardContainer from "./CardContainer"
+import Game from "./Game"
 
 function MainParent(){
+  //state for cards ⮯
   const [cards, setCards] = useState([])
-
+  //fetch request ⮯
   useEffect(() => {
     fetch('http://localhost:3001/all')
     .then(r => r.json())
     .then(data => setCards(data))
     .catch(err => console.error(err))
   }, [])
+  //state for game to start ⮯ 
+  // const [gameStart, setGameStart] = useState(false)
+  // const handleStartGame = (e) => setGameStart(currentValue => !currentValue)
 
   return (
     <div>
-      <CardContainer cards={cards}/>
+      <Game cards={cards}/>
     </div>
   )
 }
